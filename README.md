977ddd5bf4ed032a84f10a19d3f9ba59e48b154d
Christian John <cjohn@ucdavis.edu>
2021-01-04 15:28:15 -0800

    Initial code dump

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# contextMaps
-Generate high-definition contextual maps
+# Generate high-definition contextual maps
+
+This repository contains code for accessing, filtering, and downloading satellite-derived imagery (using Google Earth Engine), and plotting that imagery for contextual maps (using R and the power of ggplot) to be used in publications and presentations. 
+
+Imagery can be accessed, trimmed to a Region of Interest (ROI), cleaned of poor-quality data and downloaded via Earth Engine using the contents of the `scripts/getData.txt` script in your Earth Engine code editor. For your convenience, the data that are generated through that script are available in the `data/` folder.
+
+Once you've got your data, you can use the `scripts/plotMaps.R` script to generate minimalist maps that provide context to your audience. Below are two examples: the first is an image of Yolo County, California (the `scripts/` folder includes materials to reproduce exactly this image); the second is an image of California with the Sierra Nevada ecoregion highlighted in light grey (challenge: can you reproduce this image on your own?).
+
+<img src="plots/contextPlot.jpg" alt="Example map of Yolo County, CA" height="500"/>
+<img src="examples/CA_context.jpg" alt="Example map of CA with Sierra Ecoregion" height="500"/>
+
+
---
+++ scripts/plotMaps.R
@@ -0,0 +1,96 @@
+
+#### Import packages ----
+library(tidyverse)
+library(raster)
+library(sf)
+library(RStoolbox)
+library(cowplot)
+
+
+#### Import data ----
+## Raster
+eeImg = raster::stack("data/Yolo_LC08_RGB_2020.tif")
+eeImg
+
+## Vector
+## Yolo County
+ROI = sf::read_sf("data/Yolo_ROI.shp")
+ROI
+
+## CA State
+CA = USAboundaries::us_states() %>%
+  st_as_sf() %>%
+  filter(name == "California")
+CA
+
+
+#### Data wrangling ----
+## Raster
+# Convert to data.frame
+imgFortFull = fortify(eeImg, maxpixels = 5e7)
+imgFort = imgFortFull %>%
+  rename("R" = vis.red,
+         "G" = vis.green,
+         "B" = vis.blue) %>%
+  filter(R + G + B != 0) %>%
+  mutate(Rsc = scales::rescale(R, to = c(0,1), from = c(0,255)),
+         Gsc = scales::rescale(G, to = c(0,1), from = c(0,255)),
+         Bsc = scales::rescale(B, to = c(0,1), from = c(0,255)))
+
+
+#### Plots ----
+## Vector context plot
+vectorPlot = ggplot() +
+  geom_sf(data = CA,
+          fill = "grey90") +
+  geom_sf(data = ROI,
+          fill = "grey60") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "transparent", 
+                                        colour = "transparent"),  
+        plot.background = element_rect(fill = "transparent", 
+                                       colour = "transparent"))
+vectorPlot
+
+
+## RGB plot
+rgbPlot = ggplot() +
+  geom_tile(data = imgFort, 
+              aes(x = x, y = y, 
+                  fill = rgb(Rsc,Gsc,Bsc)))  +
+  scale_fill_identity() +
+  geom_sf(data = ROI,
+          fill = "transparent",
+          lwd = 1.5,
+          col = "grey40") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "transparent", 
+                                        colour = "transparent"),  
+        plot.background = element_rect(fill = "transparent", 
+                                       colour = "transparent"))
+rgbPlot
+
+
+
+#### Combine contextual vector and RGB imagery to a single plot with inset
+## Generate a "complete" plot that includes contextual vector data
+completePlot = ggdraw() +
+  draw_plot(rgbPlot) +
+  draw_plot(vectorPlot, 
+            x = 0.075, y = 0.075, 
+            width = 0.4, height = 0.5)
+completePlot
+
+
+
+#### Save plots ----
+## Give some thought to the output metadata:
+# A 4k display is 3840 x 2160 pixels. Therefore there's really no need to have presentation images be >3840 horizontal pixels or >2160 vertical pixels
+# Let's do some math. If a figure is to take up half of a ppt slide, then we can happily take it to 1920 horizontal pixels. If that's the case, we can make final image dimensions 7x7 inches, with a resolution of 300 pixels per inch ("dpi") and lose no more information than we would from 4k viewing anyway (final dimensions 2100 x 2100 pixels).
+
+ggsave(filename = "plots/rgbonlyPlot.jpg",
+       rgbPlot, bg = "transparent",
+       height = 7, width = 7, units = "in", dpi = 300)
+ggsave(filename = "plots/contextPlot.jpg",
+       completePlot, bg = "transparent",
+       height = 7, width = 7, units = "in", dpi = 300)
